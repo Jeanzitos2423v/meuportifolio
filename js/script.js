6e747641020e4dd4200e15692124b2d9d499cf91
Jeanzitos2423v <187412185+Jeanzitos2423v@users.noreply.github.com>
2024-11-28 12:40:09 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Adiciona funcionalidade ao botão
+document.getElementById("linkButton").addEventListener("click", function() {
+    window.open("https://pt.wikipedia.org/wiki/Socialismo", "_blank");
+});
